e7c639be9b7a4d0dbd22e6ed24575eebdf2e0311
megapig9001 <megapig9001@gmail.com>
2024-01-01 23:52:06 -0800

    Dependency fix
    Test fix

--- SteamboatWillie/patch.js
+++ SteamboatWillie/patch.js
@@ -1,12 +1,12 @@
 {
 	"dependencies": [
 		"nmlgc/base_tsa",
-        "thpatch/lang_en/th12"
+        "thpatch/lang_en"
 	],
 	"id": "SteamboatWillie",
 	"servers": [
 		"https://raw.githubusercontent.com/megapig9001/megapig9001-mods/main/SteamboatWillie/"
 	],
-	"title": "TH12. Replaces Nazrins Sprites with Steamboat Willie (Drawn by ZzzZleepy). Also edits some dialouge.",
+	"title": "TH12. Replaces Nazrins Sprites with Steamboat Willie (Drawn by Zzzzoozie). Also edits some dialouge.",
 	"update": true
 }
