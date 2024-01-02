b397ff627531caf09617a9bba1ee7d90f66e1ea0
megapig9001 <megapig9001@gmail.com>
2024-01-01 20:16:41 -0800

    Added Main

--- SteamboatWillie/patch.js
+++ SteamboatWillie/patch.js
@@ -5,7 +5,7 @@
 	],
 	"id": "SteamboatWillie",
 	"servers": [
-		"https://raw.githubusercontent.com/megapig9001/megapig9001-mods/SteamboatWillie/"
+		"https://raw.githubusercontent.com/megapig9001/megapig9001-mods/main/SteamboatWillie/"
 	],
 	"title": "TH12. Replaces Nazrins Sprites with Steamboat Willie (Drawn by ZzzZleepy). Also edits some dialouge.",
 	"update": true
