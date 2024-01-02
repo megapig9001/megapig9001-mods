81121e6613e853ada23ec215d64c6feba5a64544
megapig9001 <megapig9001@gmail.com>
2024-01-01 20:24:24 -0800

    Create repo.js
    Added the Repo

---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+    "id": "Megapig9001",
+    "title": "Megapig9001 Mods",
+    "contact": "Discord Megapig9001",
+    "servers": [
+        "https://raw.githubusercontent.com/megapig9001/megapig9001-mods/main/"
+    ],
+    "patches": {
+        "SteamboatWillie: TH12. Replaces Nazrins Sprites with Steamboat Willie (Drawn by ZzzZleepy). Also edits some dialouge.",
+    }
+}
